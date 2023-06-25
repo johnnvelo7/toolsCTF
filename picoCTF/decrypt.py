ef0eaef9c974903703b49b6fb57e8b73430699d5
John Velo <80282650+johnnvelo7@users.noreply.github.com>
2023-06-26 00:18:00 +0200

    Add files via upload

---
+++ picoCTF/decrypt.py
@@ -0,0 +1,5 @@
+code_bytes = (bytes('灩捯䍔䙻ㄶ形楴獟楮獴㌴摟潦弸彥㜰㍢㐸㙽','u16')[2:]) 
+
+code = code_bytes.decode('utf-8')
+
+print(code)
---
+++ picoCTF/decrypt2.py
@@ -0,0 +1,4 @@
+obfuscated_str = 'ipocTC{F61b_ti_snits43_dfo8_e_07b384}6'
+original_str = obfuscated_str[::-1]
+print(original_str)
+
